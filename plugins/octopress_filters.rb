729c23905019ef0ea55723496460796a5e1e50c3
Paul Annesley <paul@annesley.cc>
2011-09-24 15:53:33 +1000

    Date ordinalize changed: Jun 21st, 1982 => 21st June 1982

--- plugins/octopress_filters.rb
+++ plugins/octopress_filters.rb
@@ -99,10 +99,10 @@ module OctopressLiquidFilters
     date
   end
 
-  # Returns an ordidinal date eg July 22 2007 -> July 22nd 2007
+  # Returns an ordidinal date eg July 22 2007 -> 22nd July 2007
   def ordinalize(date)
     date = datetime(date)
-    "#{date.strftime('%b')} #{ordinal(date.strftime('%e').to_i)}, #{date.strftime('%Y')}"
+    "#{ordinal(date.strftime('%e').to_i)} #{date.strftime('%B')} #{date.strftime('%Y')}"
   end
 
   # Returns an ordinal number. 13 -> 13th, 21 -> 21st etc.
